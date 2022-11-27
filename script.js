7e4ae51aa3d70d3cffd0731c0f5769bef933ff15
adebowale <ademibowaleadegboye@gmail.com>
2022-11-27 09:15:01 +0100

    implement hamburger menu toggle active and hide

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function mobileMenu() {
   if (navMenu.classList.contains('hide')) {
     navMenu.classList.remove('hide');
     hamburger.classList.toggle('active');
-  
+  } else {
+    navMenu.classList.add('hide');
+    hamburger.classList.toggle('active');
+  }
 }
 hamburger.addEventListener('click', mobileMenu);
 /* For closing navigation on the click of each link */
