561dcbd00fe1d5bd02f7fa0aa8ceadd5c7e9ee15
adebowale <ademibowaleadegboye@gmail.com>
2022-11-27 09:14:04 +0100

    removed bugs

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ function mobileMenu() {
   if (navMenu.classList.contains('hide')) {
     navMenu.classList.remove('hide');
     hamburger.classList.toggle('active');
-  } else {
-    navMenu.classList.add('hide');
-    hamburger.classList.toggle('active');
-  }
+  
 }
 hamburger.addEventListener('click', mobileMenu);
 /* For closing navigation on the click of each link */
